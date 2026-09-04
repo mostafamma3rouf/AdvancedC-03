c8adce4635afa45df7af99a0c60b718e8500480b
mostafamma3rouf <mostafamohamedmarouf@gmail.com>
2026-09-04 22:55:17 +0300

    Q3

--- Program.cs
+++ Program.cs
@@ -63,37 +63,91 @@ namespace AdvancedC_03
                 Safely get the player with score 999
                 Remove the player with score 200 and print the updated list
              */
-            Dictionary<int, string> leaderboard = new Dictionary<int, string> // Collection of dictionary
-            {
-                [500] = "Ahmed",
-                [200] = "Sara",
-                [800] = "Ali",
-                [350] = "Mona"
-            };
+            //Dictionary<int, string> leaderboard = new Dictionary<int, string> // Collection of dictionary
+            //{
+            //    [500] = "Ahmed",
+            //    [200] = "Sara",
+            //    [800] = "Ali",
+            //    [350] = "Mona"
+            //};
 
-            Console.WriteLine($"First key: {leaderboard.Keys.First()}, First value: {leaderboard.Values.First()}");
-            Console.WriteLine("------------------------------");
+            //Console.WriteLine($"First key: {leaderboard.Keys.First()}, First value: {leaderboard.Values.First()}");
+            //Console.WriteLine("------------------------------");
 
-            bool IsExists = leaderboard.ContainsKey(500);         // Check if score 500 exists
-            Console.WriteLine($"Contains score 500: {IsExists}");
-            Console.WriteLine("------------------------------");
+            //bool IsExists = leaderboard.ContainsKey(500);         // Check if score 500 exists
+            //Console.WriteLine($"Contains score 500: {IsExists}");
+            //Console.WriteLine("------------------------------");
 
-            if (leaderboard.TryGetValue(999, out string? player))  // Safely get the player with score 999
-            {
-                Console.WriteLine($"Player with score 999: {player}");
-            }
-            else
-            {
-                Console.WriteLine("Player with score 999 not found.");
-            }
-            Console.WriteLine("------------------------------");
+            //if (leaderboard.TryGetValue(999, out string? player))  // Safely get the player with score 999
+            //{
+            //    Console.WriteLine($"Player with score 999: {player}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Player with score 999 not found.");
+            //}
+            //Console.WriteLine("------------------------------");
 
-            leaderboard.Remove(200);    // Remove the player with score 200
-            foreach (var players in leaderboard)
-            {
-                Console.WriteLine($"Score: {players.Key}, Player: {players.Value}");
-            }
-            Console.WriteLine("------------------------------");
+            //leaderboard.Remove(200);    // Remove the player with score 200
+            //foreach (var players in leaderboard)
+            //{
+            //    Console.WriteLine($"Score: {players.Key}, Player: {players.Value}");
+            //}
+            //Console.WriteLine("------------------------------");
+            #endregion
+
+            #region Exercise 3: Phone Book
+            /*
+             * Build a phone book application.
+                Create a Collection  with 4 contacts (name → phone number)
+                Add a new contact using [] syntax (add or update)
+                Try adding a duplicate using .Add() — catch the exception and print the error
+                Try adding a duplicate using .TryAdd() — print whether it succeeded
+                Search for a contact that doesn’t exist
+                Get a contact with a fallback of "Not Found"
+                Print all Keys on one line, then all Values on another line
+             */
+            //Dictionary<string, string> phoneBook = new Dictionary<string, string>
+            //{
+            //    ["Ahmed"] = "01038047759",
+            //    ["Sara"] = "01038047759",
+            //    ["Menna"] = "01012840954",
+            //    ["Mostafa"] = "01115936686"
+            //};
+
+            //phoneBook["Ali"] = "01038047759";  //using [] syntax add
+
+            //Console.WriteLine("------------------------------");
+
+            //try                                // Try adding a duplicate using .Add()
+            //{
+            //    phoneBook.Add("Mostafa", "01115936686");
+            //}
+            //catch (ArgumentException ex)       // catch the exception and print the error
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //Console.WriteLine("------------------------------");
+
+            //bool added =phoneBook.TryAdd("Mostafa", "01115936686"); // Try adding a duplicate using .TryAdd()
+            //Console.WriteLine($"TryAdd succeeded: {added}");        // succeeded is added or not
+            //Console.WriteLine("------------------------------");
+
+            //bool IsExists = phoneBook.ContainsKey("Mona"); // Search for a contact that doesn’t exist
+            //Console.WriteLine($"Contains contact 'Mona': {IsExists}");
+            //Console.WriteLine("------------------------------");
+
+            //string phoneNumber = phoneBook.GetValueOrDefault("Aya", "Not Found"); // Get a contact with a fallback of "Not Found"
+            //Console.WriteLine($"Aya phone: {phoneNumber}");
+            //Console.WriteLine("------------------------------");
+
+            //foreach (var key in phoneBook.Keys)     // Print all Keys on one line
+            //    Console.Write(key + ", ");
+
+            //Console.WriteLine();
+
+            //foreach (var value in phoneBook.Values) // Print all Values on another line
+            //    Console.Write(value + ", ");
             #endregion
         }
     }
